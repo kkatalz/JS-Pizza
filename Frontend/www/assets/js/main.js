dd86460200f704748979fa032ab8f220d1a8161d
kkatalz <zlata.kiev2004@gmail.com>
2023-06-25 16:42:11 +0300

    local storage

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -1,3 +1,190 @@
+const pizza_info = [
+  {
+    id: 1,
+    icon: "assets/images/pizza_7.jpg",
+    title: "Імпреза",
+    type: "М’ясна піца",
+    content: {
+      meat: ["балик", "салямі"],
+      chicken: ["куриця"],
+      cheese: ["сир моцарелла", "сир рокфорд"],
+      pineapple: ["ананаси"],
+      additional: ["томатна паста", "петрушка"],
+    },
+    small_size: {
+      weight: 370,
+      size: 30,
+      price: 99,
+    },
+    big_size: {
+      weight: 660,
+      size: 40,
+      price: 169,
+    },
+    is_new: true,
+    is_popular: true,
+  },
+  {
+    id: 2,
+    icon: "assets/images/pizza_2.jpg",
+    title: "BBQ",
+    type: "М’ясна піца",
+    content: {
+      meat: ["мисливські ковбаски", "ковбаски папероні", "шинка"],
+      cheese: ["сир домашній"],
+      mushroom: ["шампінйони"],
+      additional: ["петрушка", "оливки"],
+    },
+    small_size: {
+      weight: 460,
+      size: 30,
+      price: 139,
+    },
+    big_size: {
+      weight: 840,
+      size: 40,
+      price: 199,
+    },
+    is_popular: true,
+  },
+  {
+    id: 3,
+    icon: "assets/images/pizza_1.jpg",
+    title: "Міксовий поло",
+    type: "М’ясна піца",
+    content: {
+      meat: ["вітчина", "куриця копчена"],
+      cheese: ["сир моцарелла"],
+      pineapple: ["ананаси"],
+      additional: ["кукурудза", "петрушка", "соус томатний"],
+    },
+    small_size: {
+      weight: 430,
+      size: 30,
+      price: 115,
+    },
+    big_size: {
+      weight: 780,
+      size: 40,
+      price: 179,
+    },
+  },
+  {
+    id: 4,
+    icon: "assets/images/pizza_5.jpg",
+    title: "Сициліано",
+    type: "М’ясна піца",
+    content: {
+      meat: ["вітчина", "салямі"],
+      cheese: ["сир моцарелла"],
+      mushroom: ["шампінйони"],
+      additional: ["перець болгарський", "соус томатний"],
+    },
+    small_size: {
+      weight: 450,
+      size: 30,
+      price: 111,
+    },
+    big_size: {
+      weight: 790,
+      size: 40,
+      price: 169,
+    },
+  },
+  {
+    id: 17,
+    icon: "assets/images/pizza_3.jpg",
+    title: "Маргарита",
+    type: "Вега піца",
+    content: {
+      cheese: ["сир моцарелла", "сир домашній"],
+      tomato: ["помідори"],
+      additional: ["базилік", "оливкова олія", "соус томатний"],
+    },
+    small_size: {
+      weight: 370,
+      size: 30,
+      price: 89,
+    },
+  },
+  {
+    id: 43,
+    icon: "assets/images/pizza_6.jpg",
+    title: "Мікс смаків",
+    type: "М’ясна піца",
+    content: {
+      meat: ["ковбаски"],
+      cheese: ["сир моцарелла"],
+      mushroom: ["шампінйони"],
+      pineapple: ["ананаси"],
+      additional: ["цибуля кримська", "огірки квашені", "соус гірчичний"],
+    },
+    small_size: {
+      weight: 470,
+      size: 30,
+      price: 115,
+    },
+    big_size: {
+      weight: 780,
+      size: 40,
+      price: 180,
+    },
+  },
+  {
+    id: 90,
+    icon: "assets/images/pizza_8.jpg",
+    title: "Дольче Маре",
+    type: "Морська піца",
+    content: {
+      ocean: [
+        "криветки тигрові",
+        "мідії",
+        "ікра червона",
+        "філе червоної риби",
+      ],
+      cheese: ["сир моцарелла"],
+      additional: ["оливкова олія", "вершки"],
+    },
+    big_size: {
+      weight: 845,
+      size: 40,
+      price: 399,
+    },
+  },
+  {
+    id: 6,
+    icon: "assets/images/pizza_4.jpg",
+    title: "Россо Густо",
+    type: "Морська піца",
+    content: {
+      ocean: ["ікра червона", "лосось копчений"],
+      cheese: ["сир моцарелла"],
+      additional: ["оливкова олія", "вершки"],
+    },
+    small_size: {
+      weight: 400,
+      size: 30,
+      price: 189,
+    },
+    big_size: {
+      weight: 700,
+      size: 40,
+      price: 299,
+    },
+  },
+];
+// local storage
+let storedOrderList = JSON.parse(localStorage.getItem("orderList"));
+let storedFilterPosition = JSON.parse(localStorage.getItem("filter"));
+
+let orderListItems = storedOrderList ? [...storedOrderList] : [];
+
+function saveToLocalStorage() {
+  localStorage.setItem("orderList", JSON.stringify(orderListItems));
+}
+function saveFilterToLocalStorage(filter) {
+  localStorage.setItem("filter", JSON.stringify(filter));
+}
 // filters
 const filterElements = document.querySelector(".main-pizza-filters");
 filterElements.addEventListener("click", filterPizza);
@@ -15,18 +202,24 @@ function filterPizza(event) {
     addPizzasToPanel(
       pizza_info.filter((pizza) => pizza.type === "М’ясна піца")
     );
+    saveFilterToLocalStorage("М'ясні");
   } else if (filterText === "З ананасами") {
     addPizzasToPanel(pizza_info.filter((pizza) => pizza.content.pineapple));
+    saveFilterToLocalStorage("З ананасами");
   } else if (filterText === "З грибами") {
     addPizzasToPanel(pizza_info.filter((pizza) => pizza.content.mushroom));
+    saveFilterToLocalStorage("З грибами");
   } else if (filterText === "З морепродуктами") {
     addPizzasToPanel(
       pizza_info.filter((pizza) => pizza.type === "Морська піца")
     );
+    saveFilterToLocalStorage("З морепродуктами");
   } else if (filterText === "Вега") {
     addPizzasToPanel(pizza_info.filter((pizza) => pizza.type === "Вега піца"));
+    saveFilterToLocalStorage("Вега");
   } else {
     addPizzasToPanel(pizza_info);
+    saveFilterToLocalStorage("");
   }
 }
 
@@ -146,13 +339,30 @@ function addToBasket() {
           isAlreadyInOrder = true;
           const amountContainer =
             titleElement.nextElementSibling.nextElementSibling;
-
           const amountElement = amountContainer.querySelector("#pizza-amount");
+
+          //recalculate single pizza price
+          const pizzaPriceContainer =
+            amountElement.previousElementSibling.previousElementSibling;
+          const pizzaPriceText = pizzaPriceContainer.querySelector("span");
+
+          console.log(pizzaPriceText);
+          pizzaPriceText.innerText =
+            parseInt(pizzaPriceText.innerText) +
+            parseInt(pizzaPriceText.innerText) /
+              parseInt(amountElement.innerText);
+
           amountElement.innerText = parseInt(amountElement.innerText) + 1;
+
+          orderListItems = orderListItems.map((item) =>
+            item.title === wholePizzaTitle
+              ? { ...item, amount: item.amount + 1 }
+              : item
+          );
+          saveToLocalStorage();
         }
       });
 
-      // console.log(pizzaSizeElement);
       const pizzaAmount = 1;
 
       const pizzaReceipt = document.getElementById("receipt-to-pay");
@@ -160,6 +370,16 @@ function addToBasket() {
         parseInt(pizzaReceipt.innerText) + parseInt(pizzaPrice);
 
       if (!isAlreadyInOrder) {
+        orderListItems.push({
+          title: wholePizzaTitle,
+          size: pizzaSize,
+          weight: parseInt(pizzaWeight),
+          amount: parseInt(pizzaAmount),
+          price: parseInt(pizzaPrice),
+          imageSrc: imageSrc,
+        });
+
+        saveToLocalStorage();
         //count pizzas in order list
         const pizzasAmount = document.getElementById("order-list-pizza-amount");
         pizzasAmount.innerText++;
@@ -173,7 +393,7 @@ function addToBasket() {
               <p><img src="./assets/images/weight.svg" alt="weight" />${pizzaWeight}</p>
             </div>
             <div class="pizza-list-item-price_amount">
-            <h3>${pizzaPrice}грн</h3>
+            <h3><span id="pizza-item-price">${pizzaPrice}</span>грн</h3>
               <button class="pizza-list-item-add_delete" id="decrease-pizza" type="button">-</button>
               <h3 id="pizza-amount">${pizzaAmount}</h3>
               <button class="pizza-list-item-add_delete green" id="increase-pizza" type="button">
@@ -197,182 +417,6 @@ function getAllValuesSeparatedByComma(obj) {
   console.log(values);
   return values.join(", ");
 }
-const pizza_info = [
-  {
-    id: 1,
-    icon: "assets/images/pizza_7.jpg",
-    title: "Імпреза",
-    type: "М’ясна піца",
-    content: {
-      meat: ["балик", "салямі"],
-      chicken: ["куриця"],
-      cheese: ["сир моцарелла", "сир рокфорд"],
-      pineapple: ["ананаси"],
-      additional: ["томатна паста", "петрушка"],
-    },
-    small_size: {
-      weight: 370,
-      size: 30,
-      price: 99,
-    },
-    big_size: {
-      weight: 660,
-      size: 40,
-      price: 169,
-    },
-    is_new: true,
-    is_popular: true,
-  },
-  {
-    id: 2,
-    icon: "assets/images/pizza_2.jpg",
-    title: "BBQ",
-    type: "М’ясна піца",
-    content: {
-      meat: ["мисливські ковбаски", "ковбаски папероні", "шинка"],
-      cheese: ["сир домашній"],
-      mushroom: ["шампінйони"],
-      additional: ["петрушка", "оливки"],
-    },
-    small_size: {
-      weight: 460,
-      size: 30,
-      price: 139,
-    },
-    big_size: {
-      weight: 840,
-      size: 40,
-      price: 199,
-    },
-    is_popular: true,
-  },
-  {
-    id: 3,
-    icon: "assets/images/pizza_1.jpg",
-    title: "Міксовий поло",
-    type: "М’ясна піца",
-    content: {
-      meat: ["вітчина", "куриця копчена"],
-      cheese: ["сир моцарелла"],
-      pineapple: ["ананаси"],
-      additional: ["кукурудза", "петрушка", "соус томатний"],
-    },
-    small_size: {
-      weight: 430,
-      size: 30,
-      price: 115,
-    },
-    big_size: {
-      weight: 780,
-      size: 40,
-      price: 179,
-    },
-  },
-  {
-    id: 4,
-    icon: "assets/images/pizza_5.jpg",
-    title: "Сициліано",
-    type: "М’ясна піца",
-    content: {
-      meat: ["вітчина", "салямі"],
-      cheese: ["сир моцарелла"],
-      mushroom: ["шампінйони"],
-      additional: ["перець болгарський", "соус томатний"],
-    },
-    small_size: {
-      weight: 450,
-      size: 30,
-      price: 111,
-    },
-    big_size: {
-      weight: 790,
-      size: 40,
-      price: 169,
-    },
-    is_new: true,
-  },
-  {
-    id: 17,
-    icon: "assets/images/pizza_3.jpg",
-    title: "Маргарита",
-    type: "Вега піца",
-    content: {
-      cheese: ["сир моцарелла", "сир домашній"],
-      tomato: ["помідори"],
-      additional: ["базилік", "оливкова олія", "соус томатний"],
-    },
-    small_size: {
-      weight: 370,
-      size: 30,
-      price: 89,
-    },
-  },
-  {
-    id: 43,
-    icon: "assets/images/pizza_6.jpg",
-    title: "Мікс смаків",
-    type: "М’ясна піца",
-    content: {
-      meat: ["ковбаски"],
-      cheese: ["сир моцарелла"],
-      mushroom: ["шампінйони"],
-      pineapple: ["ананаси"],
-      additional: ["цибуля кримська", "огірки квашені", "соус гірчичний"],
-    },
-    small_size: {
-      weight: 470,
-      size: 30,
-      price: 115,
-    },
-    big_size: {
-      weight: 780,
-      size: 40,
-      price: 180,
-    },
-  },
-  {
-    id: 90,
-    icon: "assets/images/pizza_8.jpg",
-    title: "Дольче Маре",
-    type: "Морська піца",
-    content: {
-      ocean: [
-        "криветки тигрові",
-        "мідії",
-        "ікра червона",
-        "філе червоної риби",
-      ],
-      cheese: ["сир моцарелла"],
-      additional: ["оливкова олія", "вершки"],
-    },
-    big_size: {
-      weight: 845,
-      size: 40,
-      price: 399,
-    },
-  },
-  {
-    id: 6,
-    icon: "assets/images/pizza_4.jpg",
-    title: "Россо Густо",
-    type: "Морська піца",
-    content: {
-      ocean: ["ікра червона", "лосось копчений"],
-      cheese: ["сир моцарелла"],
-      additional: ["оливкова олія", "вершки"],
-    },
-    small_size: {
-      weight: 400,
-      size: 30,
-      price: 189,
-    },
-    big_size: {
-      weight: 700,
-      size: 40,
-      price: 299,
-    },
-  },
-];
 
 // increase / decrease amount in pizza list order
 function handlePizzaAmount() {
@@ -381,37 +425,83 @@ function handlePizzaAmount() {
       (event.target && event.target.id === "increase-pizza") ||
       event.target.id === "decrease-pizza"
     ) {
+      const parentNode = event.target.closest(".pizza-list-item");
+
+      const pizzaTitle = parentNode.querySelector(
+        ".pizza-list-item-title"
+      ).innerText;
+
       if (event.target && event.target.id === "decrease-pizza") {
         const pizzaAmount = event.target.nextElementSibling.innerText;
 
         //recalculate pizza's receipt
-        const pizzaPrice = event.target.previousElementSibling.innerText;
+        const pizzaPriceContainer = event.target.previousElementSibling;
+        console.log(pizzaPriceContainer);
+        const pizzaPrice = pizzaPriceContainer.querySelector("span");
+
         const pizzaReceipt = document.getElementById("receipt-to-pay");
         pizzaReceipt.innerText =
-          parseInt(pizzaReceipt.innerText) - parseInt(pizzaPrice);
+          parseInt(pizzaReceipt.innerText) -
+          parseInt(pizzaPrice.innerText) / parseInt(pizzaAmount);
 
         if (pizzaAmount != 1) {
           event.target.nextElementSibling.innerText = parseInt(pizzaAmount) - 1;
+
+          orderListItems = orderListItems.map((item) =>
+            item.title === pizzaTitle
+              ? { ...item, amount: parseInt(item.amount) - 1 }
+              : item
+          );
+          saveToLocalStorage();
         } else {
-          const parentNode = event.target.closest(".pizza-list-item");
           parentNode.remove();
+
+          orderListItems = orderListItems.filter(
+            (item) => item.title !== pizzaTitle
+          );
+          saveToLocalStorage();
           const pizzasAmount = document.getElementById(
             "order-list-pizza-amount"
           );
           pizzasAmount.innerText--;
         }
+
+        //calculate single pizza's price (189*3)
+
+        pizzaPrice.innerText =
+          parseInt(pizzaPrice.innerText) -
+          parseInt(pizzaPrice.innerText) / parseInt(pizzaAmount);
       } else {
         const pizzaAmount = event.target.previousElementSibling.innerText;
         event.target.previousElementSibling.innerText =
           parseInt(pizzaAmount) + 1;
 
-        //recalculate pizza's receipt
-        const pizzaPrice =
+        //calculate single pizza's price (189*3)
+        const pizzaPriceContainer =
           event.target.previousElementSibling.previousElementSibling
-            .previousElementSibling.innerText;
+            .previousElementSibling;
+        console.log(pizzaPriceContainer);
+        const pizzaPrice = pizzaPriceContainer.querySelector("span");
+
+        //recalculate pizza's receipt
         const pizzaReceipt = document.getElementById("receipt-to-pay");
         pizzaReceipt.innerText =
-          parseInt(pizzaReceipt.innerText) + parseInt(pizzaPrice);
+          parseInt(pizzaReceipt.innerText) +
+          parseInt(pizzaPrice.innerText) / parseInt(pizzaAmount);
+
+        console.log(parseInt(pizzaAmount));
+        console.log(parseInt(pizzaPrice.innerText));
+        pizzaPrice.innerText =
+          parseInt(pizzaPrice.innerText) +
+          parseInt(pizzaPrice.innerText) / parseInt(pizzaAmount);
+
+        //save to local storage
+        orderListItems = orderListItems.map((item) =>
+          item.title === pizzaTitle
+            ? { ...item, amount: parseInt(item.amount) + 1 }
+            : item
+        );
+        saveToLocalStorage();
       }
     }
   });
@@ -422,6 +512,10 @@ function handleRemovingPizza() {
   document.addEventListener("click", (event) => {
     if (event.target && event.target.id === "remove-pizza") {
       const parentNode = event.target.closest(".pizza-list-item");
+      const pizzaTitle = parentNode.querySelector(
+        ".pizza-list-item-title"
+      ).innerText;
+      console.log(pizzaTitle);
       parentNode.remove();
       const pizzasAmount = document.getElementById("order-list-pizza-amount");
       pizzasAmount.innerText--;
@@ -433,9 +527,14 @@ function handleRemovingPizza() {
       const pizzaAmount =
         event.target.previousElementSibling.previousElementSibling.innerText;
       const pizzaReceipt = document.getElementById("receipt-to-pay");
+
       pizzaReceipt.innerText =
-        parseInt(pizzaReceipt.innerText) -
-        parseInt(pizzaPrice) * parseInt(pizzaAmount);
+        parseInt(pizzaReceipt.innerText) - parseInt(pizzaPrice);
+
+      orderListItems = orderListItems.filter(
+        (item) => item.title !== pizzaTitle
+      );
+      saveToLocalStorage();
     }
   });
 }
@@ -460,12 +559,92 @@ function clearOrderList() {
     //recalculate pizza's receipt
     const pizzaReceipt = document.getElementById("receipt-to-pay");
     pizzaReceipt.innerText = 0;
+
+    orderListItems = [];
+    saveToLocalStorage();
   });
 }
 
-addPizzasToPanel(pizza_info);
+function initialCardRender() {
+  const pizzaListOrder = document.getElementById("pizza-list-order");
+  const pizzaOrderAmount = document.getElementById("order-list-pizza-amount");
+
+  const pizzaReceipt = document.getElementById("receipt-to-pay");
+
+  orderListItems.forEach((pizza) => {
+    const pizzaCardInOrder = `
+        <div class="pizza-list-item">
+            <img class="pizza-list-item-image" src="${pizza.imageSrc}" />
+            <h4 class="pizza-list-item-title">${pizza.title}</h4>
+            <div class="pizza-list-item-icons">
+            <p><img src="./assets//images//size-icon.svg" alt="size" />${
+              pizza.size
+            }</p>
+              <p><img src="./assets/images/weight.svg" alt="weight" /> ${
+                pizza.weight
+              }</p>
+            </div>
+            <div class="pizza-list-item-price_amount">
+            <h3><span id="pizza-item-price">${
+              pizza.price * pizza.amount
+            }</span>грн</h3>
+              <button class="pizza-list-item-add_delete" id="decrease-pizza" type="button">-</button>
+              <h3 id="pizza-amount">${pizza.amount}</h3>
+              <button class="pizza-list-item-add_delete green" id="increase-pizza" type="button">
+                +
+              </button>
+              <button class="pizza-list-item-add_delete delete" id="remove-pizza" type="button">
+              +
+              </button>
+              </div>
+      </div>
+      `;
+    pizzaListOrder.innerHTML += pizzaCardInOrder;
+
+    pizzaReceipt.innerText =
+      parseInt(pizzaReceipt.innerText) + pizza.price * pizza.amount;
+  });
+  pizzaOrderAmount.innerText = orderListItems.length;
+}
+
+function initialFilterRender() {
+  const filters = document.querySelectorAll(".main-pizza-filter");
+  filters.forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  filterText = storedFilterPosition || "";
+
+  if (filterText === "М'ясні") {
+    addPizzasToPanel(
+      pizza_info.filter((pizza) => pizza.type === "М’ясна піца")
+    );
+    filters[1].classList.add("active");
+  } else if (filterText === "З ананасами") {
+    addPizzasToPanel(pizza_info.filter((pizza) => pizza.content.pineapple));
+    filters[2].classList.add("active");
+  } else if (filterText === "З грибами") {
+    addPizzasToPanel(pizza_info.filter((pizza) => pizza.content.mushroom));
+    filters[3].classList.add("active");
+  } else if (filterText === "З морепродуктами") {
+    addPizzasToPanel(
+      pizza_info.filter((pizza) => pizza.type === "Морська піца")
+    );
+    filters[4].classList.add("active");
+  } else if (filterText === "Вега") {
+    addPizzasToPanel(pizza_info.filter((pizza) => pizza.type === "Вега піца"));
+    filters[5].classList.add("active");
+  } else {
+    addPizzasToPanel(pizza_info);
+    filters[0].classList.add("active");
+  }
+}
+
+initialCardRender();
 
 addToBasket();
 handlePizzaAmount();
 handleRemovingPizza();
 clearOrderList();
+
+initialFilterRender();
